24d328843ce6e9db4104b5692bf35809b38ee87b
Jarek Brzozowski <45065772+j-brzozowski@users.noreply.github.com>
2024-06-04 11:39:54 +0200

    Update files (#92)

--- survicate_widget.d.ts
+++ survicate_widget.d.ts
@@ -15,7 +15,6 @@ export enum SurveyType {
   PageSurvey = 'PageSurvey',
   MobileSurvey = 'MobileSurvey',
   FeedbackButton = 'FeedbackButton',
-  FeedbackButtonSurvey = 'FeedbackSurvey',
   IntercomSurvey = 'IntercomSurvey'
 }
 
